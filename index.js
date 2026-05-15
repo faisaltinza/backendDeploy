bfd59386d9f8034a9d86fe080c8bd8df19cb648a
faisaltinza <faisaltinza@gmail.com>
2026-05-16 00:10:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello !')
+})
+
+app.get('/hey', (req, res) => {
+    res.send("<h1>NIkal pehli fursat me </h1>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
